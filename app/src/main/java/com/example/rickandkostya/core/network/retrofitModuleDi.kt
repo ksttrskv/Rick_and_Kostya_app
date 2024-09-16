5ce5ba4ba13650788ca8d3d90290e133ab620f47
patrin artem <patrin.artem2@wb.ru>
2024-09-16 21:00:53 +0300

    version 1

--- app/src/main/java/com/example/rickandkostya/core/network/retrofitModuleDi.kt
+++ app/src/main/java/com/example/rickandkostya/core/network/retrofitModuleDi.kt
@@ -1,48 +1,26 @@
 package com.example.rickandkostya.core.network
 
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 val retrofitModule = module {
     single {
         Retrofit.Builder()
             .baseUrl("https://rickandmortyapi.com/api/")
-            .client(getOkHttpClient())
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create()) // replace kotlin serialization
             .build()
     }
+
 }
 
-
-data class Timeouts(
-    val call: Long = 120,
-    val connect: Long = 5,
-    val read: Long = 40,
-    val write: Long = 15
-)
-
-fun getOkHttpClient(
-    vararg interceptors: Interceptor,
-    timeouts: Timeouts = Timeouts()
-): OkHttpClient {
-    // val loggingInterceptor = HttpLoggingInterceptor(ApiLogger()).apply {
-    //     level = HttpLoggingInterceptor.Level.BODY
-    // }
-
-    return OkHttpClient()
-        .newBuilder()
-        .apply {
-            interceptors.forEach {
-                addInterceptor(it)
-            }
-            // if (BuildConfig.DEBUG) addInterceptor(loggingInterceptor)
-            callTimeout(timeouts.call, TimeUnit.SECONDS)
-            connectTimeout(timeouts.connect, TimeUnit.SECONDS)
-            readTimeout(timeouts.read, TimeUnit.SECONDS)
-            writeTimeout(timeouts.write, TimeUnit.SECONDS)
-        }.build()
-}
+// single {
+//     val contentType = "application/json".toMediaType()
+//     Retrofit.Builder()
+//         .baseUrl("https://rickandmortyapi.com/api/")
+//         .addConverterFactory(Json {
+//             ignoreUnknownKeys = true
+//             explicitNulls = false
+//         }.asConverterFactory(contentType))
+//         .build()
+// }
